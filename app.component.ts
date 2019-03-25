4784385ae4b770a885e9f99696607372cdaadae6
std84 <vizen84@gmail.com>
2019-03-25 20:58:21 +0200

    Add files via upload

--- app.component.ts
+++ app.component.ts
@@ -6,10 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'accordion';
-  users = [
-    { id: 1001, label: 'Roma', lat: 42, lng: 12 },
-    { id: 1002, label: 'Copenaghen', lat: 56, lng: 14},
-    { id: 1003, label: 'Gibuti', lat: 11, lng: 44}
-  ];
+  title = 'primengtest';
 }
